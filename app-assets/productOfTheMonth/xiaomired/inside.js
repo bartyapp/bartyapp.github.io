a9b541d7e187e4a9edc676ff780bcbaa0bb91a86
Semih Kılıçgedik <55982250+bartyapp@users.noreply.github.com>
2021-08-06 14:44:02 +0300

    xiaomi

---
+++ app-assets/productOfTheMonth/xiaomired/inside.js
@@ -0,0 +1,92 @@
+const productOfTheMonth = {
+    productNumber: 8,
+    active: true,
+    isProductOfTheMonth: true,
+    started: false,
+    startAfter: 100,
+    //startAfter: 1,
+    dateText: "Ağustos 2021",
+    productOfTheMonthId: "00000007",
+    realProductId: "60b654407e7843183193f6c4",//"608bc549a16637073348aaf6",//606596c0ec7f8361b9fedad7, 60400e11fc3f8225245c301b, 5fd0f2d3c933da51d0bb370c,//6016bad0216a1142a60bf5ca
+    title: "Xiaomi Redmi 9c 64 GB",
+    brand: "Xiaomi",
+    notStartedMainText: `Kampanya 5 Ağustos'da Başlayacak`,
+    startTime: "2021-08-05T00:00:00",
+    dueDateAndTime: "2021-09-05T20:00:00",
+    initialBarty: 54000,
+    barty: 43200,
+    //initialBarty: 43200,
+    decreaseRate: 0,
+    images: [
+        "https://www.barty.app/app-assets/productOfTheMonth/jbl/main.jpg?cache_delete=3",
+        "https://www.barty.app/app-assets/productOfTheMonth/jbl/inside.png?cache_delete=3",
+    ],
+    texts: {
+        currentPrice: "Ürünün fiyatı",
+        productOfTheMonthTop: "%20 indirim ile",
+        promotionalProductTop: `Her ay popüler ürünlere muhteşem indirimler ile sahip olman çok yakın.\n<b>Haydi, yarışmaya şimdi katıl!<b>`,
+        promotionalProductInitialValue: `Birinci el fiyatı:`,
+        bartyTlConversionText: "TL Karşılığı:",
+        promotionsCountDownTitle: "Kampanyanın bitmesine kalan süre:",
+        endOfTheCampaign: `Kampanya sona erdi!`,
+        endOfTheCampaignText1: `Ürünü Leaderboard sıralamasına göre öncelikli alma hakkı olan kişiye bilgilendirme e-postası attık.`,
+        endOfTheCampaignText2: `Yeni kampanyamız yeni ürünle birlikte <b>01.08.2021<b> burada!`,
+        promotionalProductBought: `Fırsat Kaçtı`,
+        promotionalProductBoughtText1: `Kampanyalı ürün alıcısı ile buluştu.`,
+        promotionalProductBoughtText2: `Yeni kampanyamız yeni ürünle birlikte <b>06.09.2021<b> burada!\nBizi takip edin!`,
+        seeProductDetail: `Ürün Detaylarını Gör`,
+        goBackToBartyMain: `Barty Pazar'a Dön`,
+        buyNow: `Hemen Al`,
+        goToLeaderBoard: `LeaderBoard'a Katıl`,
+        campaignBannerTitle: `Kampanya ve kullanım detayları`,
+        promotionalCongrats: `Tebrikler`,
+        promotionalBoughtModalText: `Ürünü en uygun fiyatıyla aldın. En kısa zamanda seninle iletişime geçip ürünü sana ulaştıracağız.`,
+        closePromotionalBoughtModal: `Kampanya Sayfasına Dön`,
+        promotionalCantBuyModalTitle: `Ürünü alamadın :(`,
+        promotionalCantBuyModalText: `Ürünü alabilmek için yeterli Barty Puan’a sahip değilsin.
+Butona dokun, eksik Barty Puan’ını şimdi tamamla.`,
+        promotionalCantBuyModalButtonText: `Barty Satın Al`,
+        promotionalTitle: `Ayın Ürünü Yarışması Nedir?`,
+        promotionalText1: `Her ay avantajlı şartlarla birbirinden popüler ürünleri almak istersen bu yarışma tam sana göre!`,
+        promotionalText2: `Yapman gereken çok basit. <b>Leaderboard'a katıl<b> ve ay içerisinde bol bol takas yaparak liderliğe yüksel.`,
+        promotionalText3: `Ay sonunda, Leaderboard sıralamasına göre ürünü %20 indirim ile 43.200 Barty Puan’a alma şansına sahip ol! \nNe kadar çok yüksek değerli takas yaparsan
+        liderliğe yükselebilirsin. Yarışma ile ilgili bilgiler Kampanya Detay'da.\nBol şanslar! :)`,
+        promotionalProductBoughtBanner: `ÜRÜN\nALINDI!`,
+        campaignDetailsHeader: `Kampanya Detayları`,
+        campaignDetailLongText: `
+- Kampanya 05.08.2021 - 05.09.2021 tarihleri arasında geçerlidir.
+
+- Leaderboard’daki sıraya göre katılımcılar ürünü 43.200 Barty Puan’a satın alma şansını elde edilir.
+
+- Kazanma şansını elde eden kişiye kayıtlı e-posta adresine bilgilendirme mesajı atılır.
+
+- Kişiye 5 iş günü içerisinde ulaşılamazsa satın alma hakkı bir sonraki kişiye aktarılır.
+
+- Leaderboard sıralaması, Barty kullanıcılarının takas aktifliğine göre puanlandırılır. Sıralama puanlamaları aşağıdaki noktalara göre yapılır: 
+
+    . Kampanya döneminde yapılan takas işlemleri dikkate alınır.
+    . Takaslanan ürünün değeri fazla olursa daha fazla puan kazanırsın. 
+    . Daha çok takas tamamlarsan daha fazla puan kazanırsın.
+
+- Beraberlik durumunda ayın ürünü kampanyası döneminde yapılan takas sayısı dikkate alınacaktır.
+
+- Ürünü alma hakkı en üst sıradaki kişiden aşağıya doğru önceliklendirilir. Örneğin, 1. olan kullanıcı ürünü almak istemezse, ürünü alma hakkı 2. kullanıcıya aktarılır.
+
+- Leaderboard’da ilk 10 kişi ürünü indirimli satın alma şansına sahip olabilme hakkı ile sınırlandırılmıştır. Örneğin, Leaderboard sıralamasındaki 10. kişi ürünü almak istemez ise ürünü alma hakkı 11. kişiye aktarılmayacaktır.
+
+- Her ay yeni kampanya ile Leaderboard yenilenir.
+
+- Kampanyadaki ürün sıfırdır. Ürün 1 adet ile sınırlıdır.
+
+- Barty Mobil İnternet A.Ş. her zaman tek taraflı olarak kampanyayı sürekli veya geçici olarak durdurma ve kampanya koşullarında herhangi bir uyarı olmaksızın değişiklik yapma hakkını saklı tutar. Yazım hataları bağlayıcı değildir.
+
+- Şüpheli işlem tespit edilen kullanıcıların (dolandırıcılık, gerçek olmayan takas, vb.) ürünü alma hakkı düşürülebilir. 
+
+- Alıcı cayma hakkı saklıdır.
+
+- Katılımcılar kampanyaya katılarak kampanya koşullarını eksiksiz kabul etmiş sayılırlar.`,
+    }
+}
+module.exports = {
+    productOfTheMonth
+}
